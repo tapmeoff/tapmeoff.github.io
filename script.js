e1fe732c11e59e81797ec191c107e1d4b8dfb56a
syd-nguyen <syd.tl.nguyen@gmail.com>
2025-08-07 18:31:17 -0400

    fifth commit
    issa big one (made it pretty, added stuff, etc)

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
-document.getElementById("generate").addEventListener("click", (event) => generate());
+document.getElementById("tapMeOff").addEventListener("click", (event) => tapMeOff());
 document.getElementById("playPrev").addEventListener("click", (event) => playPrev());
+document.getElementById("setting").addEventListener("click", (event) => openSettings());
+document.getElementById("dimmer").addEventListener("click", (event) => closeSettings());
 
 const a = document.getElementById("audio");
 
 const darkmode = document.getElementById("darkmode");
 darkmode.addEventListener("change", (event) => setMode());
+const SPENCER = document.getElementById("SPENCER");
+SPENCER.addEventListener("change", (event) => spence());
 
 const sheet = document.getElementById("sheet");
-const sheetmusic = document.getElementById("sheetmusic");
 const disable = document.getElementById("disable");
 const beyonce = document.getElementById("beyonce");
 const clave = document.getElementById("clave");
@@ -24,7 +27,7 @@ let prev = "";
 
 let debug = true;
 
-/* addEventListener("keypress", (event) => pauseUnpause()); */
+setMode();
 
 function pauseUnpause() {
     if (a.paused) a.play();
@@ -36,36 +39,101 @@ function tap(name) {
     a.play();
 }
 
+function openSettings() {
+    document.getElementById("settings").style.visibility = "visible";
+    document.getElementById("dimmer").style.visibility = "visible";
+}
+
+function closeSettings() {
+    /* and also close music and stop it */
+    document.getElementById("settings").style.visibility = "hidden";
+    document.getElementById("dimmer").style.visibility = "hidden";
+    hideSheet();
+    audio.load();
+}
+
+function spence() {
+    if (SPENCER.checked) {
+        document.getElementById("tapMeOff").innerHTML = "TAP ME OFF, SPENCER";
+        document.getElementById("playPrev").innerHTML = "PLAY PREVIOUS";
+        document.getElementById("setting").innerHTML = "SETTINGS";
+        document.getElementById("spencerImg").style.visibility = "visible";
+        document.getElementById("buttonGrid").style.gridTemplateAreas =
+            ' "tapMeOff tapMeOff tapMeOff" "spencer spencer spencer" "spencer spencer spencer" "spencer spencer spencer" "spencer spencer spencer" "spencer spencer spencer" "spencer spencer spencer" "spencer spencer spencer" "spencer spencer spencer" "spencer spencer spencer" "spencer spencer spencer" "spencer spencer spencer" "spencer spencer spencer" "spencer spencer spencer" "spencer spencer spencer" "spencer spencer spencer" "spencer spencer spencer" "spencer spencer spencer" "spencer spencer spencer" "spencer spencer spencer" "spencer spencer spencer" "spencer spencer spencer" "spencer spencer spencer" "spencer spencer spencer" "spencer spencer spencer" "spencer spencer spencer" "spencer spencer spencer" "spencer spencer spencer" "spencer spencer spencer" "spencer spencer spencer" "playPrev playPrev settings" ';
+    } else {
+        document.getElementById("tapMeOff").innerHTML = "tap me off";
+        document.getElementById("playPrev").innerHTML = "play previous";
+        document.getElementById("setting").innerHTML = "settings";
+        document.getElementById("spencerImg").style.visibility = "hidden";
+        document.getElementById("buttonGrid").style.gridTemplateAreas =
+            ' "tapMeOff tapMeOff tapMeOff" "playPrev playPrev settings"';
+    }
+}
+
+function showSheet(name) {
+    document.getElementById("dimmer").style.visibility = "visible";
+    if (name == "Beyonce") document.getElementById("beyonceSheet").style.visibility = "visible";
+    else if (name == "Clave") document.getElementById("claveSheet").style.visibility = "visible";
+    else if (name == "Cooper") document.getElementById("cooperSheet").style.visibility = "visible";
+    else if (name == "CrunchTime") document.getElementById("crunchTimeSheet").style.visibility = "visible";
+    else if (name == "ElTigre") document.getElementById("elTigreSheet").style.visibility = "visible";
+    else if (name == "Jamaica") document.getElementById("jamaicaSheet").style.visibility = "visible";
+    else if (name == "Mouth") document.getElementById("mouthSheet").style.visibility = "visible";
+    else if (name == "OCP") document.getElementById("ocpSheet").style.visibility = "visible";
+    else document.getElementById("speedSheet").style.visibility = "visible";
+}
+
+function hideSheet() {
+    imgs = document.querySelectorAll("img");
+    for (let i = 0; i < imgs.length; i++) imgs[i].style.visibility = "hidden";
+    document.getElementById("dimmer").style.visibility = "hidden";
+    if (SPENCER.checked) document.getElementById("spencerImg").style.visibility = "visible";
+    else document.getElementById("spencerImg").style.visibility = "hidden";
+}
+
 function setMode() {
     if (darkmode.checked) {
-        document.querySelector("body").style.backgroundColor = "black";
-        textcolor = "white";
+        backgroundColor = "var(--blue)";
+        tapColor = "var(--orange)";
+        otherButtonColor = "white";
+        otherButtonText = "var(--blue)";
     } else {
-        document.querySelector("body").style.backgroundColor = "white";
-        textcolor = "black";
+        backgroundColor = "white";
+        tapColor = "var(--orange)";
+        otherButtonColor = "var(--blue)";
+        otherButtonText = "white";
     }
-    labels = document.querySelectorAll("label");
-    for (let i=0; i<labels.length; i++)
-        labels[i].style.color = textcolor;
+    document.querySelector("body").style.backgroundColor = backgroundColor;
+    document.querySelector("#tapMeOff").style.backgroundColor = tapColor;
+    document.querySelector("#tapMeOff").style.borderColor = tapColor;
+    document.querySelector("#playPrev").style.backgroundColor = otherButtonColor;
+    document.querySelector("#playPrev").style.borderColor = otherButtonColor;
+    document.querySelector("#playPrev").style.color = otherButtonText;
+    document.querySelector("#setting").style.backgroundColor = otherButtonColor;
+    document.querySelector("#setting").style.borderColor = otherButtonColor;
+    document.querySelector("#setting").style.color = otherButtonText;
 }
 
-function waitThenDisable() {
+function waitThenCheck() {
     /* if still playing, wait 100 ms then check again */
-    if (a.ended == false) window.setTimeout(waitThenDisable, 100);
+    if (a.ended == false) window.setTimeout(waitThenCheck, 100);
     /* if ended, then disable */ else {
-        if (tracks[num] == "Beyonce") beyonce.checked = false;
-        else if (tracks[num] == "Clave") clave.checked = false;
-        else if (tracks[num] == "Cooper") cooper.checked = false;
-        else if (tracks[num] == "CrunchTime") crunchtime.checked = false;
-        else if (tracks[num] == "ElTigre") eltigre.checked = false;
-        else if (tracks[num] == "Jamaica") jamaica.checked = false;
-        else if (tracks[num] == "Mouth") mouth.checked = false;
-        else if (tracks[num] == "OCP") ocp.checked = false;
-        else speed.checked = false;
+        if (disable.checked) {
+            if (tracks[num] == "Beyonce") beyonce.checked = false;
+            else if (tracks[num] == "Clave") clave.checked = false;
+            else if (tracks[num] == "Cooper") cooper.checked = false;
+            else if (tracks[num] == "CrunchTime") crunchtime.checked = false;
+            else if (tracks[num] == "ElTigre") eltigre.checked = false;
+            else if (tracks[num] == "Jamaica") jamaica.checked = false;
+            else if (tracks[num] == "Mouth") mouth.checked = false;
+            else if (tracks[num] == "OCP") ocp.checked = false;
+            else speed.checked = false;
+        }
+        hideSheet();
     }
 }
 
-function generate() {
+function tapMeOff() {
     /* make list of tracks */
     tracks = [];
     if (beyonce.checked) tracks.push("Beyonce");
@@ -84,20 +152,24 @@ function generate() {
     num = Math.floor(Math.random() * tracks.length);
     if (debug) console.log(num);
 
-    /* show or hide sheet music */
-    if (sheet.checked) {
-        sheetMusic.style.display = "block";
-    } else sheetMusic.style.display = "none";
+    /* show sheet music if requested */
+    if (sheet.checked) showSheet(tracks[num]);
 
     /* play track */
     tap(tracks[num]);
     prev = tracks[num];
 
-    /* disable track if requested */
-    if (disable.checked) waitThenDisable();
+    /* disable track if requested and hide sheetMusic if necessary */
+    waitThenCheck();
 }
 
 function playPrev() {
     tap(prev);
     if (debug) console.log(prev);
+
+    /* show sheet music if requested */
+    if (sheet.checked) showSheet(tracks[num]);
+
+    /* disable track if requested and hide sheetMusic if necessary */
+    waitThenCheck();
 }
